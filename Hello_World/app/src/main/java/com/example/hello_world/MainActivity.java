b342d1d86c4f5103439c43117c8fa2a75cf684dd
shi428 <shi428@purdue.edu>
2019-10-19 18:48:02 -0400

    firebase

--- Hello_World/app/src/main/java/com/example/hello_world/MainActivity.java
+++ Hello_World/app/src/main/java/com/example/hello_world/MainActivity.java
@@ -3,11 +3,27 @@ package com.example.hello_world;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  private class User {
+    public User(String a, String b) {
+      this.a = a;
+      this.b = b;
     }
+    private String a;
+    private String b;
+  };
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    DatabaseReference usersRef = ref.child("users");
+    Map<String, User> users = new HashMap<>();
+    users.put("alanisawesome", new User("June 23, 1912", "Alan Turing"));
+    users.put("gracehop", new User("December 9, 1906", "Grace Hopper"));
+    usersRef.setValue(users);
+  }
 }
