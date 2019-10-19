2afb82c7b526dd6bfaa425f945d1fbd393b63aa1
comet <comet@archlinux.comet.local>
2019-10-19 18:17:44 -0500

    firebase stuff

--- Hello_World/app/src/main/java/com/example/hello_world/MainActivity.java
+++ Hello_World/app/src/main/java/com/example/hello_world/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hello_world;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,12 +21,13 @@ public class MainActivity extends AppCompatActivity {
   };
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+    myRef.setValue("Hello, World!");
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    DatabaseReference usersRef = ref.child("users");
-    Map<String, User> users = new HashMap<>();
-    users.put("alanisawesome", new User("June 23, 1912", "Alan Turing"));
-    users.put("gracehop", new User("December 9, 1906", "Grace Hopper"));
-    usersRef.setValue(users);
+
   }
 }
