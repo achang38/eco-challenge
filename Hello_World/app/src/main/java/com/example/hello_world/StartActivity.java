b96537874d847193f76e9829f78a38d4d9587a46
comet <comet@archlinux.comet.local>
2019-10-20 04:37:31 -0500

    cleanup code

--- Hello_World/app/src/main/java/com/example/hello_world/StartActivity.java
+++ Hello_World/app/src/main/java/com/example/hello_world/StartActivity.java
@@ -45,7 +45,6 @@ public class StartActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(DatabaseError error) {
-                // Failed to read value
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
