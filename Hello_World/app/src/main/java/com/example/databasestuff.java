2f88347f0d43a31be661d131e808e44cf8f87c7e
comet <comet@archlinux.comet.local>
2019-10-19 20:11:05 -0500

    so far hash

--- Hello_World/app/src/main/java/com/example/databasestuff.java
+++ Hello_World/app/src/main/java/com/example/databasestuff.java
@@ -1,4 +1,13 @@
+package com.example;
+
+import android.util.Log;
+
 import com.example.hello_world.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -7,33 +16,43 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+
+import static android.content.ContentValues.TAG;
 
 public class databasestuff {
-  public void readData() {
-  }
-  public void writeData() {
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("users");
-    //Map <String, User> =
-  }
-  public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm)
-  {
-    // Create a list from elements of HashMap
-    List<Map.Entry<String, Integer> > list =
-            new LinkedList<Map.Entry<String, Integer> >(hm.entrySet());
+  private DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("users");
+  Map<String, User> users = new HashMap<>();
 
-    // Sort the list
-    Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
-      public int compare(Map.Entry<String, Integer> o1,
-                         Map.Entry<String, Integer> o2)
+  public void readData() {
+
+  }
+
+
+  public void writeData() {
+    for(int i = 0; i < 5; i++){
+      int randomId = new Random().nextInt();
+      int randomUser = new Random().nextInt();
+      int randomScore = new Random().nextInt();
+      users.put(String.valueOf(randomId), new User(String.valueOf(randomUser), randomScore));
+    }
+    myRef.setValue(users);
+  }
+
+  public static HashMap<String, User> sortByValue(HashMap<String, User> hm)
+  {
+    List<Map.Entry<String, User> > list = new LinkedList<Map.Entry<String, User> >(hm.entrySet());
+
+    Collections.sort(list, new Comparator<Map.Entry<String, User> >() {
+      public int compare(Map.Entry<String, User> o1,
+                         Map.Entry<String, User> o2)
       {
-        return (o1.getValue()).compareTo(o2.getValue());
+        return (new Integer(o1.getValue().getScore())).compareTo(new Integer(o2.getValue().getScore()));
       }
     });
 
-    // put data from sorted list to hashmap
-    HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
-    for (Map.Entry<String, Integer> aa : list) {
+    HashMap<String, User> temp = new LinkedHashMap<String, User>();
+    for (Map.Entry<String, User> aa : list) {
       temp.put(aa.getKey(), aa.getValue());
     }
     return temp;
--- Hello_World/app/src/main/java/com/example/hello_world/User.java
+++ Hello_World/app/src/main/java/com/example/hello_world/User.java
@@ -1,11 +1,19 @@
 package com.example.hello_world;
 
 public class User {
-    public String name;
-    public int score;
+    private String name;
+    private int score;
 
     public User(String name, int score) {
         this.name = name;
         this.score = score;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getScore(){
+        return score;
     }
 };
