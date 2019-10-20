694a22a896603aefb84923d2ad70c0298a21cf62
comet <comet@archlinux.comet.local>
2019-10-20 05:12:36 -0500

    update

---
+++ Hello_World/app/src/main/java/com/example/eco_challenge/AddCommute.java
@@ -0,0 +1,105 @@
+package com.example.eco_challenge;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+
+public class AddCommute extends AppCompatActivity {
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_commute);
+
+        final RadioButton bike = findViewById(R.id.commute_bike);
+        final RadioButton carpool = findViewById(R.id.commute_carpool);
+        final RadioButton pub_transport = findViewById(R.id.commute_pub_transport);
+        final RadioButton walk = findViewById(R.id.commute_walk);
+        final TextView textView6 = findViewById(R.id.textView6);
+        Button button_submit = findViewById(R.id.button_submit);
+
+        final SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView6.setText("Minutes: " + progress);
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            });
+
+        button_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bike.isChecked()) {
+                    int points = seekBar.getProgress()/5;
+                    updateScore(points);
+                    goToStart(v);
+                }
+                else if (carpool.isChecked()) {
+                    int points = seekBar.getProgress()/6;
+                    updateScore(points);
+                    goToStart(v);
+                }
+                else if (pub_transport.isChecked()) {
+                    int points = seekBar.getProgress()/7;
+                    updateScore(points);
+                    goToStart(v);
+                }
+                else if (walk.isChecked()) {
+                    int points = seekBar.getProgress()/8;
+                    updateScore(points);
+                    goToStart(v);
+                }
+                else {
+                    goToStart(v);
+                }
+            }
+        });
+    }
+
+    public static void updateScore(final int value) {
+        FirebaseUser currUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference userRef = rootRef.child("users").child(currUser.getUid());
+        userRef.runTransaction(new Transaction.Handler() {
+            @Override
+            public Transaction.Result doTransaction(MutableData mutableData) {
+                User user = mutableData.getValue(User.class);
+                if (user == null) {
+                    return Transaction.success(mutableData);
+                }
+                user.dailyScore += value;
+                user.totalScore += value;
+                mutableData.setValue(user);
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(DatabaseError databaseError, boolean b, DataSnapshot dataSnapshot) {}
+        });
+    }
+
+    public void goToStart(View view) {
+        Intent intent = new Intent(this,StartActivity.class);
+        startActivity(intent);
+    }
+}
